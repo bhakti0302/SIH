9861bd37e8896a1b5580a83d9a3be51a8e33eaeb
bhakti0302 <bhaktichindhe@gmail.com>
2022-08-26 10:50:14 +0530

    commit1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import NavScrollExample from './components/navbar';
+import Carousel_Home from './components/carousel_home';
+import RaiseClaim from './components/raiseClaim';
+import KYC_Form from './components/kycForm';
+import Home from './components/home';
+import Login from './components/login';
+import {
+  BrowserRouter ,
+  Switch,
+  Route,
+  Link,
+  Routes,
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {/* <Login/> */}
+    {/* <KYC_Form/> */}
+  
+      <NavScrollExample/> 
+     <Home/>
+      
+     
     </div>
   );
 }
